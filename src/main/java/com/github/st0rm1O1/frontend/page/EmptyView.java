28c9a4f46d92bafa87352d016be695186cd51aaa
Kunal Vartak <thekunalvartak@gmail.com>
2024-09-15 15:19:39 +0530

    Added `compani` animation

--- src/main/java/com/github/st0rm1O1/frontend/page/EmptyView.java
+++ src/main/java/com/github/st0rm1O1/frontend/page/EmptyView.java
@@ -1,4 +1,6 @@
 package com.github.st0rm1O1.frontend.page;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -9,6 +11,13 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import de.mekaso.vaadin.addon.compani.animation.Animation;
+import de.mekaso.vaadin.addon.compani.animation.AnimationBuilder;
+import de.mekaso.vaadin.addon.compani.animation.AnimationTypes;
+import de.mekaso.vaadin.addon.compani.effect.AttentionSeeker;
+import de.mekaso.vaadin.addon.compani.effect.Delay;
+import de.mekaso.vaadin.addon.compani.effect.Repeat;
+import de.mekaso.vaadin.addon.compani.effect.Speed;
 
 @PageTitle("Empty")
 @Menu(icon = "line-awesome/svg/file.svg", order = 0)
@@ -16,10 +25,12 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 @RouteAlias(value = "blank")
 @RouteAlias(value = "idk")
 @AnonymousAllowed
+@StyleSheet(Animation.STYLES)
 public class EmptyView extends VerticalLayout {
 
     public EmptyView() {
         setSpacing(false);
+
 
 //        Image img = new Image("https://start.vaadin.com/images/empty-plant.png", "placeholder plant");
         Image img = new Image("public/empty-plant.png", "placeholder plant");
@@ -30,6 +41,17 @@ public class EmptyView extends VerticalLayout {
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
         add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+//        Animator animator = Animator.init(UI.getCurrent());
+        AnimationBuilder
+                .createBuilderFor(header)
+                .create(AnimationTypes.AttentionSeekerAnimation.class)
+                .withEffect(AttentionSeeker.growOnHover)
+                .withSpeed(Speed.normal)
+                .withDelay(Delay.noDelay)
+                .withRepeat(Repeat.Once)
+                .start();
+
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
